2fd5aba727bb95532773d792688b90c50fdee131
sbmansfield <sbmansfield@NN123-HA-06.HS.sd203.org>
2015-10-19 14:11:53 -0500

    finished rps, started circles

---
+++ Chapter 5 Practice/CircleOverlap.java
@@ -0,0 +1,26 @@
+public class CircleOverlap
+{
+   public static void main(String[] args)
+   {
+      Scanner in = new Scanner(System.in);
+
+      System.out.print("Input the radius of the first circle: ");
+      double radius1 = in.nextDouble(); 
+      double xcenter1 = 0;
+      double ycenter1 = 0;
+      System.out.print("Input the radius of the second circle: ");
+      double radius2 = in.nextDouble(); 
+      double xcenter2 = 40;
+      double ycenter2 = 0;
+
+      // Your work goes here
+      if ( Math.abs(radius1 - radius2) > 0 && Math.abs(radius1 - radius2) < 20)
+      {
+          System.out.println("The circles are disjointed");
+      }
+      else if ()
+      {
+          
+      }
+   }
+}
--- Chapter 5 Practice/RockPaperScissors.java
+++ Chapter 5 Practice/RockPaperScissors.java
@@ -70,20 +70,48 @@ public class RockPaperScissors
         System.out.println("Player 2: Choose rock, scissors, or paper:");
         String player2 = scan.next().toLowerCase(); 
         
-        String r = "rock";
-        String p = "paper";
-        String s = "scissors";
-        if (r&&r)
+        String r="rock";
+        String s="scissor"; 
+        String p="paper";
+        
+        if (player1.equals(r) && player2.equals(r))
         {
             System.out.println("It's a tie");
         }
-        else if (r && s)
+        else if (player1.equals(s) && player2.equals(s)) 
+        {
+            System.out.println("It's a tie");
+        }
+        
+        else if(player1.equals(p) && player2.equals(p)) 
+        {
+            System.out.println("It's a tie");
+        }    
+        
+        else if (player1.equals(r) && player2.equals(s))
         {
             System.out.println("Player 1 wins");
         }
-        else if (r && p)
+        else if (player1.equals(p) && player2.equals(r))
+        {
+            System.out.println("Player 1 wins");
+        }
+        else if (player1.equals(s) && player2.equals(p))
+        {
+            System.out.println("Player 1 wins");
+        }    
+        else if (player1.equals(s) && player2.equals(r))
         {
             System.out.println("Player 2 wins");
-        }
+        }    
+        else if (player1.equals(p) && player2.equals(s))
+        {
+            System.out.println("Player 2 wins");
+        }    
+        else if (player1.equals(r) && player2.equals(p))
+        {
+            System.out.println("Player 2 wins");
+        }    
+        
     }
 }
