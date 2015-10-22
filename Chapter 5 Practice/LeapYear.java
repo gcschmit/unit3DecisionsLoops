88cc030eae126b449779f14500ceeba38d1d8ea9
sbmansfield <sbmansfield@NN123-HA-06.HS.sd203.org>
2015-10-22 14:12:33 -0500

    10/22

--- Chapter 5 Practice/LeapYear.java
+++ Chapter 5 Practice/LeapYear.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class LeapYear
 {
-   public void isLeapYear()
+   public static void main(String[] args)
    {
       Scanner in = new Scanner(System.in);
 
@@ -10,9 +10,13 @@ public class LeapYear
       int year = in.nextInt();
       int remainder = year%400;
       
-      if ( remainder = 0 )
+      if ( remainder == 0 )
       {
           System.out.println(year+" is a leap year");
+      }
+      else
+      {
+          System.out.println(year+" is not a leap year");
       }
    }
 }
