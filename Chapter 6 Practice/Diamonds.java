c835a3d9bb9b08ac9a0f33857f98842c7e04fad8
sbmansfield <sbmansfield@NN123-HA-06.HS.sd203.org>
2015-10-29 14:13:32 -0500

    10/29

---
+++ Chapter 6 Practice/Diamonds.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner; 
+public class Diamonds
+{
+    public static void printDiamond()   
+    {
+        Scanner scan= new Scanner(System.in); 
+        System.out.print("Enter a number: "); 
+        int n= n.nextInt();
+        int a=n; 
+        int symbol=0; 
+        String poop= "*"; 
+        
+        while(a>=n)
+        {
+            symbol++; 
+            
+        }    
+    }   
+}  
---
+++ Chapter 6 Practice/Substring.java
@@ -0,0 +1,29 @@
+// Write a program that reads a word and prints all substrings, sorted by length. For
+// example, if the user provides the input "rum", the program prints
+// r
+// u
+// m
+// ru
+// um
+// rum
+
+import java.util.Scanner;
+
+public class Substring
+{
+    public static void printSubstring()
+    {
+        String word; 
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a word: "); 
+        word = scan.next(); 
+        
+        for (int i = 0; 
+                i < word.length(); 
+                i++)
+        {
+            System.out.println(word.substring(i, i+1));
+        }
+    }
+}
